011e730f9789ab96a1bcb1c85241adc189044c40
Virginia Varzari <varzarivirginia4@gmail.com>
2023-01-28 17:12:21 +0200

    Update the CreatePostDto class.

--- server/Posts.WebApp/Posts.Common/Dtos/CreatePostDto.cs
+++ server/Posts.WebApp/Posts.Common/Dtos/CreatePostDto.cs
@@ -2,7 +2,6 @@
 {
     public class CreatePostDto
     {
-        public int Id { get; set; }
         public string Title { get; set; }
         public string Body { get; set; }
         public int UserId { get; set; }
