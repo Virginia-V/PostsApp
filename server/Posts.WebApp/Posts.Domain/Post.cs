daebe295f4fffa6b2f32a187e620ba7793aa1e9e
Virginia Varzari <varzarivirginia4@gmail.com>
2023-01-28 13:17:48 +0200

    Initial Commit

---
+++ server/Posts.WebApp/Posts.Domain/Post.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Posts.Domain
+{
+    public class Post : BaseEntity
+    {
+    }
+}
